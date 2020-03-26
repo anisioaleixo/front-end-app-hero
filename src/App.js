3633d212380ca6ff3da04911074b845af91e7ce9
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-03-26 12:27:18 -0300

    Mudando o estado de uma constante

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
-  return <Header>Curso javascript!</Header>;
+
+  const [counter, setCounter] = useState(0);
+  //Retorna um array com duas posições [valor, função de atualização]
+
+  //Incrementando um valor
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  //Decrementando um valor
+  function decrement() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
+    </div>
+  );
 }
 
 export default App;

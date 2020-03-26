b9fda2c421cc53aa19c63ddece9fb5307c891bf7
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-03-26 13:22:22 -0300

    Arquivos de imagem

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import Logon from './pages/Logon';
 
-function App() {
-
-  const [counter, setCounter] = useState(0);
-  //Retorna um array com duas posições [valor, função de atualização]
-
-  //Incrementando um valor
-  function increment() {
-    setCounter(counter + 1);
-  }
-
-  //Decrementando um valor
-  function decrement() {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  }
+function App() {  
 
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-      <button onClick={decrement}>Decrementar</button>
-    </div>
+      <Logon />
   );
 }
 

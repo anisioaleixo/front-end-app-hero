3fca7ec59a115ced70cedf3e10da2e07cd3dd4e5
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-03-29 12:06:43 -0300

    Elementos da página

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './styles.css';
+import { FiLogIn } from 'react-icons/fi'
 
 import logo from '../../assets/logo.svg';
 import heroesimg from '../../assets/heroes.png';
 
-export default function Logon(){
+export default function Logon() {
     return (
         <div className="logon-container">
-            <session className="form">
+            <section className="form">
                 <img src={logo} alt="HeBe The Hero" />
                 <form action="#">
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID" />
-                    <button type="submit">Entrat</button>
+                    <button className="button" type="submit">Entrat</button>
                     <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro!
                     </a>
                 </form>
-            </session>
+            </section>
             <img src={heroesimg} alt="Heros" />
         </div>
     );

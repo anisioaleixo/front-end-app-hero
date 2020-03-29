6c4583ab41601fe826e949e83b1306d10895a2d0
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-03-29 15:47:50 -0300

    Página Register

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import { FiLogIn } from 'react-icons/fi'
 
@@ -14,10 +15,10 @@ export default function Logon() {
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrat</button>
-                    <a href="/register">
-                        <FiLogIn size={16} color="#E02041"/>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro!
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesimg} alt="Heros" />
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
 import './styles.css';
-//import { FiLogIn } from 'react-icons/fi'
 
-
+import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
-    return <h1>Register</h1>;
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro!
+                    </Link>
+                </section>
+                <form>
+                    <input placeholder="Nome da ONG" type="text" />
+                    <input placeholder="E-mail" type="email" />
+                    <input type="text" placeholder="Whatsapp" />
+
+                    <div className="input-group">
+                        <input type="text" placeholder="Cidade" />
+                        <input type="text" placeholder="UF" style={{ width: 80 }} />
+                    </div>
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
